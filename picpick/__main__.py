91b18e62cf1453cf0beb582336ab3e92ea197ff5
michaeldel <michaeldel@protonmail.com>
2020-04-03 21:09:51 +0200

    Add output paths and classification buttons

--- picpick/__main__.py
+++ picpick/__main__.py
@@ -6,17 +6,32 @@ from typing import List
 from .gui import MainWindow
 
 
-def path(input: List[str]) -> List[pathlib.Path]:
-    return [pathlib.Path(s) for s in input]
+def dir_path(input: str) -> pathlib.Path:
+    path = pathlib.Path(input)
+    if not path.is_dir():
+        print(input)
+        print('---')
+        print(path)
+        raise NotADirectoryError
+    return path
 
 
 parser = argparse.ArgumentParser(prog='picpick')
 parser.add_argument(
-    'input', nargs='+', type=pathlib.Path, help="Path of input file (e.g. /tmp/pic.jpg)"
+    'input',
+    nargs='+',
+    type=pathlib.Path,
+    help="Path of input file (e.g. /tmp/pic.jpg)",
+)
+parser.add_argument(
+    '--output',
+    nargs='+',
+    type=dir_path,
+    help="Path of output directory (e.g. /tmp/liked/)",
 )
 args = parser.parse_args()
 
 print(args)
 
-window = MainWindow(args.input)
+window = MainWindow(args.input, args.output)
 window.run()
--- picpick/gui.py
+++ picpick/gui.py
@@ -1,7 +1,7 @@
 import pathlib
 import tkinter as tk
 
-from typing import List
+from typing import Callable, List
 
 from PIL import Image
 
@@ -9,9 +9,13 @@ from . import widgets
 
 
 class MainWindow(tk.Tk):
-    def __init__(self, paths: List[pathlib.Path]):
-        assert len(paths) > 0
-        self._paths = paths
+    def __init__(
+        self, input_paths: List[pathlib.Path], output_paths: List[pathlib.Path]
+    ):
+        assert len(input_paths) > 0
+        assert len(output_paths) > 0
+
+        self._input_paths = input_paths
 
         super().__init__()
         self.geometry('720x480')
@@ -19,16 +23,18 @@ class MainWindow(tk.Tk):
 
         self._setup_paths_listbox()
         self._setup_image_display()
+        self._setup_picking_buttons(output_paths)
 
-        self.select_image(self._paths[0])
+        self._to_move = {}
+        self._current_input_path_index = 0
+        self.select_image(self._input_paths[self._current_input_path_index])
 
     def select_image(self, path: pathlib.Path):
-        assert path in self._paths
         self._image_display.set_image(Image.open(path))
 
     def _setup_paths_listbox(self):
-        paths_listbox = tk.Listbox(master=self)
-        paths_listbox.insert(tk.END, *self._paths)
+        paths_listbox = tk.Listbox(master=self, width=50)
+        paths_listbox.insert(tk.END, *self._input_paths)
 
         def select(event: tk.Event):
             widget = event.widget
@@ -39,11 +45,34 @@ class MainWindow(tk.Tk):
             self.select_image(pathlib.Path(widget.get(selection[0])))
 
         paths_listbox.bind('<<ListboxSelect>>', select)
-        paths_listbox.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+        paths_listbox.pack(fill=tk.BOTH, side=tk.LEFT)
 
     def _setup_image_display(self):
         self._image_display = widgets.ImageDisplay(master=self)
         self._image_display.pack(fill=tk.BOTH, expand=True)
+
+    def _setup_picking_buttons(self, output_paths: List[pathlib.Path]):
+        frame = tk.Frame(master=self, bg='red')
+        frame.pack(fill=tk.X)
+
+        def mark_to_move(output_path: pathlib.Path) -> Callable[[None], None]:
+            def inner():
+                current_input_path = self._input_paths[self._current_input_path_index]
+                self._to_move[current_input_path] = output_path
+                self._current_input_path_index += 1
+
+                if self._current_input_path_index < len(self._input_paths):
+                    self.select_image(self._input_paths[self._current_input_path_index])
+                else:
+                    print(self._to_move)
+
+            return inner
+
+        for output_path in output_paths:
+            button = tk.Button(
+                master=frame, text=output_path, command=mark_to_move(output_path),
+            )
+            button.pack(fill=tk.X, expand=True, side=tk.LEFT)
 
     def run(self):
         self.mainloop()

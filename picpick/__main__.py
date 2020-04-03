abe4c99e67b518157511f504a33bfcd08157a347
michaeldel <michaeldel@protonmail.com>
2020-04-03 11:07:53 +0200

    Add initial CLI program

--- picpick/__main__.py
+++ picpick/__main__.py
@@ -1,1 +1,17 @@
-print("Hello, world!")
+import argparse
+import pathlib
+
+from typing import List
+
+
+def path(input: List[str]) -> List[pathlib.Path]:
+    return [pathlib.Path(s) for s in input]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'input', nargs='+', type=pathlib.Path, help="Path of input file (e.g. /tmp/pic.jpg)"
+)
+args = parser.parse_args()
+
+print(args)

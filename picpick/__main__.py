5e63d69ef10e699f0527d1a5dcf71e69c4917e90
michaeldel <michaeldel@protonmail.com>
2020-05-01 16:43:40 +0200

    Make program create empty project by default

--- picpick/__main__.py
+++ picpick/__main__.py
@@ -1,39 +1,14 @@
-import argparse
-import pathlib
+import click
 
 from .controller import Controller
-from .model import Image, Model, Tag
+from .model import Model
 
 
-def dir_path(input: str) -> pathlib.Path:
-    path = pathlib.Path(input)
-    if not path.is_dir():
-        raise NotADirectoryError
-    return path
+@click.command()
+def empty():
+    model = Model()
+    controller = Controller(model=model)
+    controller.run()
 
 
-parser = argparse.ArgumentParser(prog='picpick')
-parser.add_argument(
-    'input',
-    nargs='+',
-    type=pathlib.Path,
-    help="path of input file (e.g. /tmp/pic.jpg)",
-)
-parser.add_argument(
-    '--tags', nargs='+', type=str, help="name of tag (e.g. red)",
-)
-parser.add_argument(
-    '--copy', action='store_true', help="copy the input files instead of moving them"
-)
-args = parser.parse_args()
-
-# TODO: enforce input and tags uniqueness
-model = Model()
-
-for path in args.input:
-    model.images.add(Image(path=path))
-for name in args.tags:
-    model.tags.add(Tag(name=name))
-
-controller = Controller(model=model)
-controller.run()
+empty()

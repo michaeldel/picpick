bb82d79d4e3307cd67001aa79dd8cfe32c2fe05a
michaeldel <michaeldel@protonmail.com>
2020-05-03 22:43:53 +0200

    Add version display

--- picpick/__main__.py
+++ picpick/__main__.py
@@ -4,12 +4,23 @@ from typing import Optional
 
 import click
 
+from . import __version__
 from .controller import Controller
 from .model import Model
 
 
+def version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(__version__)
+    ctx.exit()
+
+
 @click.command()
 @click.argument('filename', type=click.Path(exists=True), required=False)
+@click.option(
+    '--version', is_flag=True, callback=version, expose_value=False, is_eager=True
+)
 def run(filename: Optional[str]):
     model = Model()
     controller = Controller(model=model)
